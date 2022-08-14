c85a478b976548bb0e93d42d1c17ff3e8e5006f9
oneflyingfish <a_flying_fish@outlook.com>
2022-08-14 15:19:43 +0800

    upload

--- ModelRun/run-example.py
+++ ModelRun/run-example.py
@@ -11,6 +11,8 @@ import json
 import numpy
 import time
 
+mydriver=drivers.GPU()
+
 name="easy-model-fish"
 input_info={
     "name": "input_edge",
@@ -24,51 +26,67 @@ easy_model=onnx.load(Config.ModelSavePathName(name))
 mod, params = relay.frontend.from_onnx(easy_model,{"input_edge": (4,3,14,14)})
 irModule=relay.transform.InferType()(mod)                    # tvm.ir.module.IRModule
 
-print(irModule)
+# print(irModule)
 
-print(params.keys())
+# print(params.keys())
 
-shape_dict = {v.name_hint : v.checked_type for v in irModule["main"].params}
-for label,type_name in shape_dict.items():
-    print(label,":",type_name)
+# shape_dict = {v.name_hint : v.checked_type for v in irModule["main"].params}
+# for label,type_name in shape_dict.items():
+#     print(label,":",type_name)
 
 with tvm.transform.PassContext(opt_level=0):
-    lib = relay.build(mod, target=drivers.GPU.target, params=params)
+    lib = relay.build(mod, target=mydriver.target, params=params)
 
-module = graph_executor.GraphModule(lib["default"](drivers.GPU.device))
+module = graph_executor.GraphModule(lib["default"](mydriver.device))
 
 #############################################################################################################
 # 方法一
 test_data=None
+flag=True
+time_first=None
 with open(Config.ModelSaveDataPathName(name),'r') as fp:
     test_data=json.load(fp)
 
 start=time.time()
 for input_test,output_test in test_data.items():
     module.set_input(input_info['name'], numpy.array(eval(input_test),dtype="float32"))
+    
+    if flag:
+        time_first=time.time()
     module.run()
+    if flag:
+        print("first runtime: ",time.time()-time_first)
+        flag=False
     tvm_output = module.get_output(0).numpy().tolist()
     if abs(tvm_output[0][0]-output_test) < 10**-4:
-        print("ok")
+        continue
     else:
         print("error")
-print("way-GraphModule run time: ",time.time()-start)
+print("way-GraphModule total run time: ",time.time()-start)
 #############################################################################################################
 
 #############################################################################################################
 # 方法二
 start=time.time()
+flag=True
+time_first=None
 with tvm.transform.PassContext(opt_level=0):
-    intrp = relay.build_module.create_executor("graph", irModule, tvm.cpu(0), target=drivers.CPU.target,params=params)
+    intrp = relay.build_module.create_executor("graph", irModule, device=mydriver.device, target=mydriver.target,params=params)
 
 test_data=None
 with open(Config.ModelSaveDataPathName(name),'r') as fp:
     test_data=json.load(fp)
 
 for input_test,output_test in test_data.items():
-    tvm_output = intrp.evaluate()(numpy.array(eval(input_test),dtype="float32")).numpy().tolist()
-    if abs(tvm_output[0][0]-output_test) < 10**-4:
-        print("ok")
+    if flag:
+        time_first=time.time()
+    tvm_output = intrp.evaluate()(numpy.array(eval(input_test),dtype="float32"))
+    if flag:
+        print("first runtime: ",time.time()-time_first)
+        flag=False
+    
+    if abs(tvm_output.numpy().tolist()[0][0]-output_test) < 10**-4:
+        continue
     else:
         print("error")
 print("way-intrp run time: ",time.time()-start)
--- drivers.py
+++ drivers.py
@@ -1,10 +1,15 @@
 import tvm
 
 class CPU:
+    def __init__(self):
+        self.target=CPU.target
+        self.device=CPU.device
     target= "llvm"
     device=tvm.cpu(0)
 
 class GPU:
+    def __init__(self):
+        self.target=GPU.target
+        self.device=GPU.device
     target= "cuda"
     device=tvm.cuda(0)
-
