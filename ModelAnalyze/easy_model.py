c00204d8ab8091826495a57cc0cdb0fd44e7ae78
WANNA959 <wannazjx@163.com>
2022-08-16 02:15:18 +0000

    update

---
+++ ModelAnalyze/easy_model.py
@@ -0,0 +1,42 @@
+import os
+import json
+import onnx
+import onnxruntime
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from GenerateModels.tvm_model import get_tvm_model
+import load_data
+import tvm
+import tvm.relay as relay
+from tvm.contrib import graph_executor
+import onnxruntime as ort
+from config import Config
+import time
+import drivers
+from relayIR.relay_graph import construct_op_graph
+
+input_shape = (4, 3, 14, 14)
+input_name = "input"
+test_count = 5
+mydriver = drivers.GPU()
+
+
+def main():
+    # onnx model
+    onnx_path = Config.ModelSavePathName("easy_model")
+    x = torch.rand(*input_shape, requires_grad=True)
+    shape_dict = {input_name: x.shape}
+    # module, _ = get_tvm_model(
+    #     onnx_path, shape_dict, target=mydriver.target, dev=mydriver.device)
+
+    onnx_model = onnx.load(onnx_path)
+    mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
+    # tvm.ir.module.IRModule
+    print(type(mod))
+    construct_op_graph(mod)
+
+
+if __name__ == '__main__':
+    main()
--- ModelRun/run-resnet50.py
+++ ModelRun/run-resnet50.py
@@ -8,24 +8,24 @@ import numpy
 import time
 import load_data
 
-mydriver=drivers.CPU()
-Count=10
-name="resnet50-v2-7"
-input_dict={
-    "data": (1, 3, 224, 224)
+mydriver = drivers.GPU()
+Count = 10
+name = "resnet50-v2-7"
+input_dict = {
+    "data": (25, 3, 224, 224)
 }
 
-irModule,params,load_time= load_data.easy_load_from_onnx(name,input_dict)
-print("load model cost %ss"%(load_time))
+irModule, params, load_time = load_data.easy_load_from_onnx(name, input_dict)
+print("load model cost %ss" % (load_time))
 
 with tvm.transform.PassContext(opt_level=0):
     lib = relay.build(irModule, target=mydriver.target, params=params)
 module = graph_executor.GraphModule(lib["default"](mydriver.device))
 
-for key,shape in input_dict.items():
+for key, shape in input_dict.items():
     module.set_input(key, numpy.random.rand(*shape).astype("float32"))
 
 for _ in range(Count):
-    time_first=time.time()
+    time_first = time.time()
     module.run()
-    print("%ss"%(time.time()-time_first))
+    print("%ss" % (time.time()-time_first))
