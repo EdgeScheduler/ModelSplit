116f0c44220e3f2a33185514a428486aa087841f
oneflyingfish <a_flying_fish@outlook.com>
2022-08-17 13:38:46 +0800

    update fish

--- ModelRun/run-resnet50.py
+++ ModelRun/run-resnet50.py
@@ -8,19 +8,24 @@ import numpy
 import time
 import load_data
 
-mydriver=drivers.CPU()
-Count=10
+mydriver=drivers.GPU()
+Count=4
 name="resnet50-v2-7"
 input_dict={
-    "data": (1, 3, 224, 224)
+    "data": (10, 3, 224, 224)
 }
 
 irModule,params,load_time= load_data.easy_load_from_onnx(name,input_dict)
 print("load model cost %ss"%(load_time))
 
 with tvm.transform.PassContext(opt_level=0):
+    start_time=time.time()
     lib = relay.build(irModule, target=mydriver.target, params=params)
+    print("time build cost:",time.time()-start_time)
+
+start_time=time.time()
 module = graph_executor.GraphModule(lib["default"](mydriver.device))
+print("time cost:",time.time()-start_time)
 
 for key,shape in input_dict.items():
     module.set_input(key, numpy.random.rand(*shape).astype("float32"))
@@ -28,4 +33,5 @@ for key,shape in input_dict.items():
 for _ in range(Count):
     time_first=time.time()
     module.run()
+    print(module.get_output(0))
     print("%ss"%(time.time()-time_first))
